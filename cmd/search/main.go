271af8bf9d7efd5fff666c6d9fc8b06522b8be95
Chara-X <1719552189@qq.com>
2024-04-25 14:55:03 +0800

    Update main.go

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -5,30 +5,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/Chara-X/priority"
 	"github.com/Chara-X/search"
 )
 
 func main() {
-	if len(os.Args) > 2 {
-		var begin = time.Now()
-		var paths []string
-		filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
-			var file, _ = os.ReadFile(path)
-			if strings.Contains(string(file), os.Args[2]) {
-				paths = append(paths, path)
-			}
-			return err
-		})
-		for _, v := range paths {
-			fmt.Println(v)
-		}
-		fmt.Println(time.Since(begin))
-		return
-	}
 	var index = search.New[string, entry]()
 	filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
 		var file, _ = os.ReadFile(path)
@@ -36,16 +18,14 @@ func main() {
 		return err
 	})
 	for scanner := bufio.NewScanner(os.Stdin); scanner.Scan(); {
-		var begin = time.Now()
 		var keys = search.Keys(scanner.Text())
 		var pq = priority.New[float64, entry]()
 		for _, v := range index.Load(keys...) {
 			pq.Push(-search.Compare(keys, v.keys), v)
 		}
-		for i := 0; i < 3 && pq.Len() > 0; i++ {
+		for i := 0; i < 5 && pq.Len() > 0; i++ {
 			fmt.Println(pq.Pop().Value.value)
 		}
-		fmt.Println(time.Since(begin))
 	}
 }
 
